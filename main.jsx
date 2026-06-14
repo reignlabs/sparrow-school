a8b9d808fcc2a650d5e7af881bc791f3ea4fee47
reignlabs <nelson@nelsonyong.com>
2026-06-14 01:34:15 -0700

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SparrowSchool from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SparrowSchool />
+  </React.StrictMode>
+);
